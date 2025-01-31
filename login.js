002f2eb578741be36a4cc8b8a3281df6d5130b09
vembiyusuf <vembisaputra273@gmail.com>
2025-01-31 22:31:50 +0700

    new

---
+++ login.js
@@ -0,0 +1,24 @@
+// Cek apakah user sudah login
+if (!localStorage.getItem('isLoggedIn')) {
+    window.location.href = 'index.html';
+}
+
+// Fungsi untuk login
+function login() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    
+    // Contoh autentikasi sederhana
+    if (username === 'Vembi' && password === '181107') {
+        localStorage.setItem('isLoggedIn', 'true');
+        window.location.href = 'lampp.html'; // Redirect ke halaman utama
+    } else {
+        alert('Username atau password salah!');
+    }
+}
+
+// Fungsi untuk logout
+function logout() {
+    localStorage.removeItem('isLoggedIn');
+    window.location.href = 'index.html';
+}
